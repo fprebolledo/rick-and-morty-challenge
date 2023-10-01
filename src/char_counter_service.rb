52188bd84a6f9267c91eea20c428eac78a56d4b9
fprebolledo <fprebolledo@uc.cl>
2023-10-01 20:23:05 -0300

    feat(char_counter_service): add char counter service

---
+++ src/char_counter_service.rb
@@ -0,0 +1,42 @@
+require_relative 'rick_and_morty_client'
+
+class CharCounterService
+
+  def initialize
+    client = RickAndMortyClient.new
+    @locations = client.get_locations
+    @episodes = client.get_episodes
+    @characters = client.get_characters
+  end
+
+  def run
+    names = {'l': 'locations', 'e': 'episodes', 'c': 'characters' }
+    results = names.map { |char, resource| count_char_in_names(char.to_s, resource) }
+  
+    return results
+  end
+
+  private
+
+  def count_char_in_names(char, resource_name)
+    names = get_names(instance_variable_get("@#{resource_name}"))
+    {
+      char: char,
+      count: ocurrences(names, char),
+      resource: resource_name
+    }
+  end
+
+  def get_names(list)
+    list.map { |element| element["name"].downcase }
+  end
+
+  def ocurrences(strings, letter)
+    strings.reduce(0) do |sum, string|
+      if string.include?(letter)
+        sum += string.count(letter)
+      end
+      sum
+    end
+  end
+end
